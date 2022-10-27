2be2488e7273cf4cd16a7ccc2e246439e8d6a919
Kenneth Ellingsen <kenneth.ellingsen@if.no>
2022-10-27 09:24:21 +0200

    Add route and swagger

---
+++ minimal-api/MyMinimalApi/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{ Title = "Minimal API", Version = "v1"});
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API v1");
+});
+
+app.MapGet("/api", () => "Hello World!");
+
+app.Run();
